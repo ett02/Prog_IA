6cc19b641cfc2c81548ef23d4ff2305ad5629f56
ett02 <ettorealoe02@gmail.com>
2026-07-12 16:35:50 +0200

    generazione grafo dettagliato per comprensione

--- smartscheduler/config.py
+++ smartscheduler/config.py
@@ -41,7 +41,7 @@ ORTOOLS_SOLVER_TIME_LIMIT: int = 30  # secondi per il CpSolver interno
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-OUTPUT_DIR = os.path.join(BASE_DIR, "output_run9")
+OUTPUT_DIR = os.path.join(BASE_DIR, "output10")
 SCENARIOS_DIR = os.path.join(DATA_DIR, "scenarios")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
